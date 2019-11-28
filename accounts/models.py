7ce4e98e619e38351dca75b09b03125e9508202f
mutembeijoe <mutembeijoe@gmail.com>
2019-11-28 11:21:11 +0300

    adds a CustomUser model with an avatar an bio field that extends django's inbuilt AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(upload_to='user_avatars', default='default.jpg')
+    bio = models.TextField()
+
+
+
