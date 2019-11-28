93726ac8864d9e0394440880d967b702380c37a1
mutembeijoe <mutembeijoe@gmail.com>
2019-11-28 13:42:01 +0300

    adds a neighbourhood model

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    police_hotline= models.ArrayField(models.CharField(max_length=13, blank=True),size=2)
+    hospital_hotline= models.ArrayField(models.CharField(max_length=13, blank=True),size=2)
