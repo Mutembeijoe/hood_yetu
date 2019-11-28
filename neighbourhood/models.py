5caf9d805258ff279d161cabf9f9cee3be41359c
mutembeijoe <mutembeijoe@gmail.com>
2019-11-28 16:39:10 +0300

    adds a business class model

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -3,6 +3,25 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
+class Business(models.Model):
+    FOOD = 1
+    BEAUTY = 2
+    SOCIAL = 3
+    ENTERTAINMENT = 4
+    HOUSING = 5
+
+    BUSINESS_CATEGORIES = [
+        (FOOD, 'Food and Beverages'),
+        (BEAUTY, 'Beauty shops'),
+        (SOCIAL,'Social Amentity'),
+        (ENTERTAINMENT, 'Entertainment'),
+        (HOUSING, 'Housing'),
+    ]
+
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.PositiveSmallIntegerField(choices=BUSINESS_CATEGORIES)
 
 
 class Neighbourhood(models.Model):
@@ -10,3 +29,4 @@ class Neighbourhood(models.Model):
     location = models.CharField(max_length=200)
     police_hotline= ArrayField(models.CharField(max_length=13, blank=True),size=3, blank=True, null=True)
     hospital_hotline= ArrayField(models.CharField(max_length=13, blank=True),size=3, blank=True, null=True)
+
