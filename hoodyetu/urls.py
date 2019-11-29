ad3e61dcbd1f8b0bb913028562aacb12da1d4331
mutembeijoe <mutembeijoe@gmail.com>
2019-11-29 11:36:06 +0300

    adds a home page, its view, the base template- where bulma css framework is added and font-awesome icon

--- hoodyetu/urls.py
+++ hoodyetu/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="home.html")),
 ]
 
 if settings.DEBUG:
