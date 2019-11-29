1fa28fa1108fab7a0d1636c1a0a27a4923319d2a
mutembeijoe <mutembeijoe@gmail.com>
2019-11-29 15:47:26 +0300

    adds a image upload field in neighbourhood model

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 
 class Neighbourhood(models.Model):
+    image = models.ImageField(upload_to='neighbourhood_avatars', default='dummy_neighbourhood.jpg')
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     police_hotline= ArrayField(models.CharField(max_length=13, blank=True),size=3, blank=True, null=True)
