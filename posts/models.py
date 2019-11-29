c95b3a7fe213b72accff983058add51a6bd77b1a
mutembeijoe <mutembeijoe@gmail.com>
2019-11-29 09:10:01 +0300

    creates Post and Tags Models in posts app and registers them with the admin

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from neighbourhood.models import Neighbourhood
+from django.contrib.auth import get_user_model
 # Create your models here.
+class Tags(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tags, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
