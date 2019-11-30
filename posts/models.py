d7ac2e50a5437d901c7656a7be26892b2f2c4ff7
mutembeijoe <mutembeijoe@gmail.com>
2019-11-30 10:11:13 +0300

    modiefied __str__method on post model to return post.title rather than post.name as the corret field name

--- posts/models.py
+++ posts/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
         return reverse('home')
