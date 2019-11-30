629326440f7a2493e3c2238e83c87d2cd6c54943
mutembeijoe <mutembeijoe@gmail.com>
2019-11-30 05:58:29 +0300

    adds a realted name to neighbourhood field in  posts to easily access posts from a specific neighbourhood in the templates

--- posts/models.py
+++ posts/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, related_name='posts')
     tags = models.ManyToManyField(Tags, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
 
