f8af404ad06d93d032df8b54efbc826cd54ec196
mutembeijoe <mutembeijoe@gmail.com>
2019-11-30 15:06:16 +0300

    adds logic to implement joining a community and leaving one

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NeighbourhoodDetailView, NeighbourhoodListView, join_neighbourhood
+from .views import NeighbourhoodDetailView, NeighbourhoodListView, join_neighbourhood, leave_neighbourhood
 
 urlpatterns = [
     path('<int:pk>/',NeighbourhoodDetailView.as_view(),name='neighbourhood'),
     path('all/', NeighbourhoodListView.as_view(), name='neighbourhood_list' ),
-    path('join/<int:community_id>/',join_neighbourhood, name='join_neighbourhood' )
+    path('join/<int:community_id>/',join_neighbourhood, name='join_neighbourhood' ),
+    path('leave/<int:community_id>/',leave_neighbourhood, name='leave_neighbourhood' ),
 ]
