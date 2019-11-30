8c842c46300ba20f3b1d29728ae7200c14c519bb
mutembeijoe <mutembeijoe@gmail.com>
2019-11-30 12:16:10 +0300

    created a user update form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from .models import CustomUser
 from .forms import CustomUserCreationForm
@@ -16,3 +16,9 @@ class UserRegistrationView(CreateView):
 class UserDetailView(DetailView):
     model = CustomUser
     template_name='profile.html'
+
+class UserUpdateView(UpdateView):
+    model = CustomUser
+    template_name = 'edit_profile.html'
+    fields = ('username', 'email', 'avatar', 'bio')
+    # success_url = reverse_lazy('profile', args=[self.object.id])
