a85b09bc60dae3efe29dd3fe63180403e20127da
mutembeijoe <mutembeijoe@gmail.com>
2019-11-30 14:28:58 +0300

    adds logic for new users to join a neighbourhood

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import NeighbourhoodDetailView
+from .views import NeighbourhoodDetailView, NeighbourhoodListView, join_neighbourhood
 
 urlpatterns = [
-    path('<int:pk>/',NeighbourhoodDetailView.as_view(),name='neighbourhood')
+    path('<int:pk>/',NeighbourhoodDetailView.as_view(),name='neighbourhood'),
+    path('all/', NeighbourhoodListView.as_view(), name='neighbourhood_list' ),
+    path('join/<int:community_id>/',join_neighbourhood, name='join_neighbourhood' )
 ]
--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView, TemplateView
+from django.shortcuts import redirect
 from .models import Neighbourhood
 
 # Create your views here.
@@ -8,3 +9,18 @@ from .models import Neighbourhood
 class NeighbourhoodDetailView(DetailView):
     model = Neighbourhood
     template_name = 'neighbourhood.html'
+
+class NeighbourhoodListView(ListView):
+    model = Neighbourhood
+    template_name = 'neighbourhood_list.html'
+
+
+def join_neighbourhood(request, community_id):
+    if(request.user.neighbourhood == None ):
+        new_hood = Neighbourhood.objects.get(id=community_id)
+        request.user.neighbourhood  = new_hood
+        request.user.save()
+        return redirect('neighbourhood',pk=1)
+    else:
+        # return render(request, 'neighbourhood_list.html')
+         return redirect('neighbourhood_list')
