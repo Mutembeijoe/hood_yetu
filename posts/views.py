9f0d14ea91a75eee2c4e3c810b24320ce9d86249
mutembeijoe <mutembeijoe@gmail.com>
2019-12-02 08:59:10 +0300

    locks out post view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import CreateView
 from .models import Post
@@ -5,10 +6,11 @@ from .models import Post
 # Create your views here.
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = ('title', 'body', 'tags')
+    login_url = 'login'
 
 
     def form_valid(self, form): # new
